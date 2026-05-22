a39cefbc5ecd2a4ebd853b168f9a2a937b6d4fc3
kinnaamf <dimoje.1337@gmail.com>
2026-05-22 22:13:46 +0300

    feat: add app navbar

---
+++ app/components/AppNavbar.vue
@@ -0,0 +1,71 @@
+<template>
+  <nav class="flex-1 space-y-1 px-3 py-4">
+    <NuxtLink
+        v-for="item in navItems"
+        :key="item.to"
+        :to="item.to"
+        active-class="active"
+        class="group nav-link hover:bg-brand-secondary"
+    >
+      <component
+          :is="item.icon"
+          :size="20"
+          class="stroke-brand-muted group-hover:stroke-brand-foreground group-[.active]:stroke-brand-primary transition-colors duration-200"
+      />
+
+      <span class="text-brand-muted group-hover:text-brand-foreground group-[.active]:text-brand-primary transition-colors duration-200">
+    {{ item.title }}
+      </span>
+    </NuxtLink>
+  </nav>
+</template>
+
+<script lang="ts" setup>
+import { LucideFolderOpen, LucideLink2, LucideChartColumn, LucideSettings } from "#components";
+
+interface NavItem {
+  title: string
+  to: string
+  icon: any
+}
+
+const navItems: NavItem[] = [
+  {
+    title: 'All Files',
+    to: '/',
+    icon: LucideFolderOpen
+  },
+  {
+    title: 'Shared Links',
+    to: '/shared-links',
+    icon: LucideLink2
+  },
+  {
+    title: 'Storage Analytics',
+    to: '/storage-analytics',
+    icon: LucideChartColumn
+  },
+  {
+    title: 'Settings',
+    to: '/settings',
+    icon: LucideSettings
+  },
+]
+</script>
+
+<style scoped lang="postcss">
+.active {
+  @apply flex items-center gap-3
+  w-full rounded-xl px-3 py-2.5
+  text-sm font-medium
+  transition-colors bg-brand-primary/10 text-brand-primary
+  pointer-events-none;
+}
+
+.nav-link {
+  @apply flex items-center gap-3
+  w-full rounded-xl px-3 py-2.5
+  text-sm font-medium text-brand-muted
+  transition-colors
+}
+</style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,17 +1,18 @@
 <template>
-  <aside class="w-64 h-screen bg-brand-darkest border-r border-brand-border">
-    <div class="h-16 flex items-center justify-center gap-3 border-b border-brand-border">
-      <div class="bg-brand-primary w-9 h-9 flex items-center justify-center rounded-xl">
-        <LucideCloud></LucideCloud>
+  <aside class="w-64 h-screen bg-brand-darkest border-r border-brand-border fixed left-0 top-0 z-50 flex flex-col">
+    <div class="h-16 flex items-center px-6 gap-3 border-b border-brand-border">
+      <div class="bg-brand-primary w-9 h-9 flex items-center justify-center rounded-2xl">
+        <LucideCloud :size="20"></LucideCloud>
       </div>
       <span class="text-lg font-semibold tracking-tight">Blob Storage</span>
     </div>
-
+    <AppNavbar/>
   </aside>
 </template>
 
 <script setup lang="ts">
 
+import AppNavbar from "~/components/AppNavbar.vue";
 </script>
 
 <style scoped lang="postcss">
