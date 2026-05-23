264e919dcf8e99f0d80f20201fc23a4fce613062
kinnaamf <dimoje.1337@gmail.com>
2026-05-23 17:55:05 +0300

    feat: add uploaded files preview

--- app/composables/useFilesize.ts
+++ app/composables/useFilesize.ts
@@ -1,13 +1,28 @@
 export const useFilesize = () => {
   const getSize = (filesize: number) => {
-    let iteration = 0;
+    const units = ['KB', 'MB', 'GB']
+    let iteration = 0
 
-    while (filesize > 1024) {
-      filesize /= 1024;
-      iteration++;
+    if (filesize === 0) {
+      return {
+        fileSize: 0,
+        storageUnit: 'B'
+      }
     }
 
-    return iteration == 1 ? `${filesize.toFixed(1)} KB` : `${filesize.toFixed(1)} MB`;
+    while (filesize >= 1024 && iteration < units.length - 1) {
+      filesize /= 1024
+      iteration++
+    }
+
+    const formattedSize = iteration === 0
+      ? filesize
+      : Math.round(filesize * 10) / 10
+
+    return {
+      fileSize: formattedSize,
+      storageUnit: units[iteration - 1],
+    }
   }
 
   return {
